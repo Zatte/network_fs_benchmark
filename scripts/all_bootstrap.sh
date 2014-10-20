d723c41928d6d7dd078d49df29439fcb7ce80dc6
Mikael Rapp <micke.rapp@gmail.com>
2014-10-21 00:55:18 +0200

    Vbox guest additions

--- scripts/all_bootstrap.sh
+++ scripts/all_bootstrap.sh
@@ -2,4 +2,4 @@
 
 #Bootstrapping basic software. Not all i needed on all nodes but KISS!
 sudo apt-get update
-sudo apt-get install ssh samba nfs-kernel-server nfs-common vsftpd 
+sudo apt-get install dkms ssh samba nfs-kernel-server nfs-common vsftpd 
