62aee47a7d3cf02193d04908bc470268b6bf0bee
Mikael Rapp <micke.rapp@gmail.com>
2016-03-25 18:12:59 -0400

    Update all_bootstrap.sh

--- scripts/all_bootstrap.sh
+++ scripts/all_bootstrap.sh
@@ -4,6 +4,10 @@
 sudo apt-get update
 sudo apt-get install dkms openssh-server samba nfs-kernel-server nfs-common vsftpd iperf nginx 
 
+#Make sure we can ssh without passwords
+ssh-keygen -t rsa
+ssh-copy-id root@localhost #and other hosts.
+
 cat > /etc/hosts <<EOL
 192.168.200.2 hostA
 192.168.200.1 hostB
