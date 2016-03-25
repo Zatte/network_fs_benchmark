c35a94cfb85239c773289e7c0ab7917ccb1f651f
Mikael Rapp <micke.rapp@gmail.com>
2016-03-25 12:19:38 -0400

    add i perf

--- scripts/all_bootstrap.sh
+++ scripts/all_bootstrap.sh
@@ -2,4 +2,7 @@
 
 #Bootstrapping basic software. Not all i needed on all nodes but KISS!
 sudo apt-get update
-sudo apt-get install dkms ssh samba nfs-kernel-server nfs-common vsftpd 
+sudo apt-get install dkms openssh-server samba nfs-kernel-server nfs-common vsftpd iperf nginx
+
+
+
