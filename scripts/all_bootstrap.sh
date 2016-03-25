30a02009815d8f214a330803c84845244531ccd0
Mikael Rapp <micke.rapp@gmail.com>
2016-03-25 18:11:58 -0400

    Update all_bootstrap.sh

--- scripts/all_bootstrap.sh
+++ scripts/all_bootstrap.sh
@@ -2,7 +2,13 @@
 
 #Bootstrapping basic software. Not all i needed on all nodes but KISS!
 sudo apt-get update
-sudo apt-get install dkms openssh-server samba nfs-kernel-server nfs-common vsftpd iperf nginx
+sudo apt-get install dkms openssh-server samba nfs-kernel-server nfs-common vsftpd iperf nginx 
 
+cat > /etc/hosts <<EOL
+192.168.200.2 hostA
+192.168.200.1 hostB
+192.168.201.2 hostC
+EOL
 
+echo 1 > /proc/sys/net/ipv4/ip_forward
 
