99270da3a03dde4449eb6ed730f5fe6aa4a9df87
Mikael Rapp <micke.rapp@gmail.com>
2016-05-14 10:56:15 -0400

    Add host c config

--- scripts/network_hostC.sh
+++ scripts/network_hostC.sh
@@ -7,3 +7,7 @@ iface eth0 inet static
   netmask 255.255.255.0
   gateway 192.168.201.1
 EOL
+
+route add -net 192.168.200.0 netmask 255.255.255.0 gw 192.168.201.1 dev eth1
+
+echo "HostC" > /etc/hostname
